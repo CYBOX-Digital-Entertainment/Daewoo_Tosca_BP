9cc3a396848e151ea58d0fd60f3c93c2247ba5ff
lemon7679 <145438843+lemon7679@users.noreply.github.com>
2024-01-11 22:39:20 +0900

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 import { world , system } from "@minecraft/server"
 system.runInterval(()=>{
     for (const entity of world.getDimension(`overworld`).getEntities()) {
-        if (entity.typeId == "daewoo:tosca_gb") {
+        if (entity.typeId == "cybox:dw_tosca") {
             entity.runCommandAsync(`fill ~3 ~3 ~3 ~-3 ~-3 ~-3 air replace light_block`)
             entity.runCommandAsync(`setblock ~~~ light_block ["block_light_level":15]`)
         }
